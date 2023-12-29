04147eabfeb7a5728dd59ed28bb67355f7ed027b
Safal Shrestha <computerstha@gmail.com>
2023-12-30 00:04:29 +0545

    try to fix

--- src/app/admin/requeststore/page.jsx
+++ src/app/admin/requeststore/page.jsx
@@ -1,6 +1,4 @@
 
-import { Button } from "@mui/material";
-
 export const metadata= {
     title:"Create your own store"
 }
@@ -11,7 +9,6 @@ const UserInformationPage = () => {
     <main className=" w-full h-full">
          <div className="flex flex-col gap-9 justify-center items-center m-12">
          <h1 className="text-4xl">Request for store </h1>
-         <Button type="button"variant="contained">Get Strated</Button>
          </div>
     </main>    
     </>
